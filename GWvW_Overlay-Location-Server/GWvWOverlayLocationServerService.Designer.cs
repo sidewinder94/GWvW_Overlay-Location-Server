6db20a84c7b3736b8cd0ca7eb8b2467b6e286a8f
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-09-14 19:02:23 +0200

    Initial commit

---
+++ GWvW_Overlay-Location-Server/GWvWOverlayLocationServerService.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace GWvW_Overlay_Location_Server
+{
+    partial class GWvWOverlayLocationServerService
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "GWvW Overlay Location Service";
+            this.notifyIcon.Visible = true;
+            // 
+            // GWvWOverlayLocationServerService
+            // 
+            this.ServiceName = "GWvWOverlayLocationServerService";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
---
+++ GWvW_Overlay-Location-Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWvW_Overlay_Location_Server
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new GWvWOverlayLocationServerService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
